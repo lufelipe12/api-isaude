d8dee65dd04ba6689c13062082ac32c15be9f34b
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-03-22 16:22:01 -0300

    Fix: errors was being especified

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
-import User from "../models/user.js";
-import jwt from "jsonwebtoken";
-import Vaccine from "../models/vaccines.js";
+import User from "../models/user.js"
+import jwt from "jsonwebtoken"
+import Vaccine from "../models/vaccines.js"
 
 class UserControllers {
   static async createUser(req, res) {
@@ -17,7 +17,7 @@ class UserControllers {
         cpf,
         password,
         vaccines,
-      } = req.body;
+      } = req.body
 
       const user = await User.create({
         name,
@@ -31,86 +31,92 @@ class UserControllers {
         cpf,
         password,
         vaccines,
-      });
+      })
 
-      return res.status(201).json(user);
+      return res.status(201).json(user)
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "Algo deu errado." });
+      console.log(error)
+      return res.status(400).json({ error: "CPF ou e-mail já cadastrados." })
     }
   }
 
   static async getAllUsers(req, res) {
     try {
-      const users = await User.find();
+      const users = await User.find()
 
-      return res.json({ users });
+      return res.json({ users })
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "Algo deu errado." });
+      console.log(error)
+      return res.status(500).json({ error: "Algo deu errado." })
     }
   }
 
   static async getUserById(req, res) {
     try {
-      const { id } = req.params;
+      const { id } = req.params
 
-      const user = await User.findById(id);
+      const user = await User.findById(id)
 
-      return res.json({ user });
+      return res.json({ user })
     } catch (error) {
-      console.log(error);
+      console.log(error)
 
-      return res.status(500).json({ error: "Algo deu errado." });
+      return res.status(500).json({ error: "Algo deu errado." })
     }
   }
 
   static async updateUser(req, res) {
     try {
-      const { id } = req.params;
-      const { avatarUrl, password, vaccines } = req.body;
+      const { id } = req.params
+      const { avatarUrl, password, vaccines } = req.body
+
+      const user = await User.findById(id)
+
+      if (!user) {
+        return res.status(400).json({ error: "Usuário não encontrado" })
+      }
 
       const userUpdated = await User.findByIdAndUpdate(id, {
         avatarUrl,
         password,
         vaccines,
         new: true,
-      });
+      })
 
-      return res.status(200).json(userUpdated);
+      return res.status(200).json(userUpdated)
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "Algo deu errado." });
+      console.log(error)
+      return res.status(500).json({ error: "Algo deu errado." })
     }
   }
 
   static async deleteUser(req, res) {
     try {
-      const { id } = req.params;
+      const { id } = req.params
 
-      await User.findByIdAndRemove(id);
+      await User.findByIdAndRemove(id)
 
-      return res.status(204).json({});
+      return res.status(204).json({})
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "Algo deu errado." });
+      console.log(error)
+      return res.status(500).json({ error: "Algo deu errado." })
     }
   }
 
   static async login(req, res) {
     try {
-      const { email, password } = req.body;
+      const { email, password } = req.body
 
       const user = await User.findOne({
         email,
-      }).select("+password");
+      }).select("+password")
 
       if (!user) {
-        throw new Error("Usuário não encontrado");
+        return res.status(400).json({ error: "Usuário não existe." })
       }
 
       if (user.password !== password) {
-        throw new Error("Senha inválida.");
+        return res.status(400).json({ error: "Senha incorreta." })
       }
 
       const token = jwt.sign(
@@ -119,22 +125,26 @@ class UserControllers {
         },
         process.env.SECRET,
         { expiresIn: "1d" }
-      );
+      )
 
-      return res.json({ token, user });
+      return res.json({ token, user })
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "aconteceu algo de errado." });
+      console.log(error)
+      return res.status(500).json({ error: "aconteceu algo de errado." })
     }
   }
 
   static async createVaccineForUser(req, res) {
     try {
-      const { id } = req.params;
-      const user = await User.findById(id);
+      const { id } = req.params
+      const user = await User.findById(id)
+
+      if (!user) {
+        return res.status(400).json({ error: "Usuário não encontrado." })
+      }
 
       const { name, manufacturer, batch, applicationDate, location, nextShot } =
-        req.body;
+        req.body
 
       const vaccine = await Vaccine.create({
         name,
@@ -143,137 +153,146 @@ class UserControllers {
         applicationDate,
         location,
         nextShot,
-      });
+      })
 
-      const userVaccines = user.vaccines;
+      const userVaccines = user.vaccines
 
       const newVaccines = await User.findByIdAndUpdate(id, {
         vaccines: [...userVaccines, vaccine],
-      });
+      })
 
-      return res.status(200).json(newVaccines);
+      return res.status(200).json(newVaccines)
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "aconteceu algo de errado." });
+      console.log(error)
+      return res.status(500).json({ error: "aconteceu algo de errado." })
     }
   }
 
   static async getAllUsersVaccines(req, res) {
     try {
-      const { id } = req.params;
+      const { id } = req.params
 
-      const user = await User.findById(id);
-      const userVaccines = user.vaccines;
+      const user = await User.findById(id)
 
-      return res.json(userVaccines);
+      if (!user) {
+        return res.status(400).json({ error: "Usuário não encontrado." })
+      }
+
+      const userVaccines = user.vaccines
+
+      return res.json(userVaccines)
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "Algo deu errado." });
+      console.log(error)
+      return res.status(500).json({ error: "Algo deu errado." })
     }
   }
 
   static async getOneVaccine(req, res) {
     try {
-      const { userId, vaccineId } = req.params;
+      const { userId, vaccineId } = req.params
 
-      const user = await User.findById(userId);
+      const user = await User.findById(userId)
 
       if (!user) {
-        return res.status(404).json({ error: "Usuário não encontrado." });
+        return res.status(404).json({ error: "Usuário não encontrado." })
       }
 
-      const vaccine = await Vaccine.findById(vaccineId);
+      const vaccine = await Vaccine.findById(vaccineId)
 
       if (!vaccine) {
-        return res.status(404).json({ error: "Vacina não encontrado." });
+        return res.status(404).json({ error: "Vacina não encontrado." })
       }
 
-      return res.json(vaccine);
+      return res.json(vaccine)
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "Algo deu errado." });
+      console.log(error)
+      return res.status(500).json({ error: "Algo deu errado." })
     }
   }
 
   static async updateOneVaccine(req, res) {
     try {
-      const { userId, vaccineId } = req.params;
+      const { userId, vaccineId } = req.params
 
       const { name, manufacturer, batch, applicationDate, location, nextShot } =
-        req.body;
+        req.body
 
-      const user = await User.findById(userId);
+      const user = await User.findById(userId)
 
       if (!user) {
-        return res.status(404).json({ error: "Usuário não encontrado." });
+        return res.status(404).json({ error: "Usuário não encontrado." })
       }
 
-      const vaccine = await Vaccine.findById(vaccineId);
+      const vaccine = await Vaccine.findById(vaccineId)
 
       if (!vaccine) {
-        return res.status(404).json({ error: "Vacina não encontrada." });
+        return res.status(404).json({ error: "Vacina não encontrada." })
       }
 
-      const newVaccines = [...user.vaccines];
+      const newVaccines = [...user.vaccines]
 
       for (let i = 0; i < newVaccines.length; i++) {
         if (newVaccines[i]._id.toString() === vaccineId) {
-          newVaccines[i].name = name !== undefined ? name : newVaccines[i].name;
+          newVaccines[i].name = name !== undefined ? name : newVaccines[i].name
           newVaccines[i].manufacturer =
             manufacturer !== undefined
               ? manufacturer
-              : newVaccines[i].manufacturer;
+              : newVaccines[i].manufacturer
           newVaccines[i].batch =
-            batch !== undefined ? batch : newVaccines[i].batch;
+            batch !== undefined ? batch : newVaccines[i].batch
           newVaccines[i].applicationDate =
             applicationDate !== undefined
               ? applicationDate
-              : newVaccines[i].applicationDate;
+              : newVaccines[i].applicationDate
           newVaccines[i].location =
-            location !== undefined ? location : newVaccines[i].location;
+            location !== undefined ? location : newVaccines[i].location
           newVaccines[i].nextShot =
-            nextShot !== undefined ? nextShot : newVaccines[i].nextShot;
+            nextShot !== undefined ? nextShot : newVaccines[i].nextShot
         }
       }
 
       const userUpdated = await User.findByIdAndUpdate(userId, {
         vaccines: [...newVaccines],
         new: true,
-      });
+      })
 
-      return res.json(userUpdated);
+      return res.json(userUpdated)
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "Algo deu errado." });
+      console.log(error)
+      return res.status(500).json({ error: "Algo deu errado." })
     }
   }
 
   static async deleteVaccine(req, res) {
     try {
-      const { userId, vaccineId } = req.params;
+      const { userId, vaccineId } = req.params
 
-      const user = await User.findById(userId);
-
-      console.log(user.vaccines);
+      const user = await User.findById(userId)
 
       if (!user) {
-        return res.status(404).json({ error: "Usuário não encontrado." });
+        return res.status(404).json({ error: "Usuário não encontrado." })
+      }
+
+      const vaccine = await Vaccine.findById(vaccineId)
+
+      if (!vaccine) {
+        return res.status(404).json({ error: "Vacina não encontrada." })
       }
 
       const newVaccinesList = user.vaccines.filter(
         (vaccine) => vaccine._id.toString() !== vaccineId
-      );
+      )
 
       await User.findByIdAndUpdate(userId, {
         vaccines: [...newVaccinesList],
-      });
+      })
 
-      return res.status(204).json({});
+      return res.status(204).json({})
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: "Algo deu errado." });
+      console.log(error)
+      return res.status(500).json({ error: "Algo deu errado." })
     }
   }
 }
 
-export default UserControllers;
+export default UserControllers
